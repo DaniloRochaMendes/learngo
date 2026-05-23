e97547252c2164d8749055ff039a5ac496391237
Danilo Mendes <danrocha_123@hotmail.com>
2026-05-22 21:47:04 -0300

    primeiro código em Golang. Um Simples Hello World!

---
+++ Conceito_2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	gopher := "Declaração de Variavel"
+	fmt.Println(gopher)
+
+	tst1 := "uma frase"
+	tst2 := 20
+	fmt.Printf("tst1: %v, \n", tst1)
+	fmt.Printf("tst2: %v, %T\n", tst2, tst2)
+
+}
