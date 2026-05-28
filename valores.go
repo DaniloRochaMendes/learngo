7706c99322f7140d03716f426b22efda83058a10
Danilo Mendes <61882330+DaniloRochaMendes@users.noreply.github.com>
2026-05-28 02:01:54 +0000

    Adição de comentários explicativos

--- valores.go
+++ valores.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 )
 
-var a int
-var b float64
-var c string
-var d bool
+var a int     // Declaração de variavel para valor int
+var b float64 // Declaração de variavel para valor float64
+var c string  // Declaração de variavel para valor string
+var d bool    // Declaração de variavel para valor bool
 
-func main() {
+func main() { //print para variaveis sem valor atribuido
 	fmt.Printf("%v, var de 'a' é %T\n", a, a)
 	fmt.Printf("%v, var de 'b' é %T\n", b, b)
 	fmt.Printf("%v, var de 'c' é %T\n", c, c)
